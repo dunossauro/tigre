fe4f7a3bfb72b525215962b7bd83cc849b095007
dunossauro <mendesxeduardo@gmail.com>
2019-12-12 23:47:35 -0300

    tests for check default capabilities

---
+++ tests/test_remote_webdrivers.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from tigre.remote.webdrivers import firefox, chrome, opera
+
+
+class TestFirefoxCapabilities(TestCase):
+    def setUp(self):
+        self.browser = firefox()
+
+    def test_should_has_version_capability(self):
+        self.assertEqual(
+            self.browser.capabilities,
+            {"browserName": "firefox"}
+        )
+
+
+class TestOperaCapabilities(TestCase):
+    def setUp(self):
+        self.browser = opera()
+
+    def test_should_has_version_capability(self):
+        self.assertEqual(
+            self.browser.capabilities,
+            {"browserName": "opera"}
+        )
+
+
+class TestChromeCapabilities(TestCase):
+    def setUp(self):
+        self.browser = chrome()
+
+    def test_should_has_version_capability(self):
+        self.assertEqual(
+            self.browser.capabilities,
+            {"browserName": "chrome"}
+        )
