efc9815fba1131e4a92d8e08daeb8908838d3c02
dunossauro <mendesxeduardo@gmail.com>
2019-12-19 13:36:19 -0300

    Remove unused capabities method from builder

--- tigre/core/meta.py
+++ tigre/core/meta.py
@@ -4,10 +4,6 @@ from abc import abstractmethod, ABC
 class Builder(ABC):
     """Builder interface."""
 
-    # @abstractproperty
-    # def capabilities(self):
-    #     ...
-
     @abstractmethod
     def build(self):
         """Build method should return a webdriver."""
