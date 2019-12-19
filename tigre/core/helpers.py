a41e554434205604a5b064c33e176f225c2f8bd2
dunossauro <mendesxeduardo@gmail.com>
2019-12-19 13:35:21 -0300

    run black on tigre

--- tigre/core/helpers.py
+++ tigre/core/helpers.py
@@ -5,9 +5,12 @@ CAPABILITIES = ['version', 'enableVNC', 'enableVideo', 'screenResolution']
 
 def attr_to_caps(attr: str) -> str:
     """Convert simpified attribute name to CamelCase capability."""
-    return defaultdict(lambda: attr, {
-        'version': 'version',
-        'vnc': 'enableVNC',
-        'video': 'enableVideo',
-        'resolution': 'screenResolution',
-    })[attr]
+    return defaultdict(
+        lambda: attr,
+        {
+            'version': 'version',
+            'vnc': 'enableVNC',
+            'video': 'enableVideo',
+            'resolution': 'screenResolution',
+        },
+    )[attr]
