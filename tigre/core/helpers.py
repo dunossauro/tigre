9445bd8fbaf4216586f6b8fa83ea7587b736ecae
dunossauro <mendesxeduardo@gmail.com>
2020-03-18 17:16:59 -0300

    adding platformName  native conveted support closes #25

--- tigre/core/helpers.py
+++ tigre/core/helpers.py
@@ -1,16 +1,19 @@
 from collections import defaultdict
 
-CAPABILITIES = ['version', 'enableVNC', 'enableVideo', 'screenResolution']
+_converted_capabilities = {
+    'version': 'version',
+    'vnc': 'enableVNC',
+    'video': 'enableVideo',
+    'resolution': 'screenResolution',
+    'platform': 'platformName',
+}
+
+CAPABILITIES = _converted_capabilities.values()
 
 
-def attr_to_caps(attr: str) -> str:
+def attr_to_caps(attr: str, capabilities=_converted_capabilities) -> str:
     """Convert simpified attribute name to CamelCase capability."""
     return defaultdict(
         lambda: attr,
-        {
-            'version': 'version',
-            'vnc': 'enableVNC',
-            'video': 'enableVideo',
-            'resolution': 'screenResolution',
-        },
+        capabilities,
     )[attr]
